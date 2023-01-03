c8d8d7e12eea6f0b82373c3d475d49c0e9482c1e
Wise4lyf <wisdoms85@yahoo.com>
2023-01-03 08:26:01 -0800

    2 print

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char in range(26):
-    print("{:s}".format(chr(char + ord("a"))), end="")
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='*
